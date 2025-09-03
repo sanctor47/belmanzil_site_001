34d655e92578673feae6079bc251536b876bd19d
sanctor47 <aser.e.nabil@gmail.com>
2025-09-03 09:52:48 +0300

    feat: add robots and sitemap functions for SEO optimization

---
+++ src/app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+    return [
+        {
+            url: 'https://belmanzil.com',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://belmanzil.com/story',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://belmanzil.com/team',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://belmanzil.com/booking',
+            lastModified: new Date(),
+        },
+    ];
+}
